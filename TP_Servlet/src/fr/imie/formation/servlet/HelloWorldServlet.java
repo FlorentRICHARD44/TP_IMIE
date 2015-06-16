21592f8e23a6b95075909211ea3a3b5bd3ad37fb
Florent RICHARD <florentrichard@hotmail.fr>
2015-06-16 14:12:49 +0200

    TP Servlet: ajout links

--- TP_Servlet/src/fr/imie/formation/servlet/HelloWorldServlet.java
+++ TP_Servlet/src/fr/imie/formation/servlet/HelloWorldServlet.java
@@ -35,16 +35,6 @@ public class HelloWorldServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /* (non-Javadoc)
-     * @see javax.servlet.GenericServlet#init()
-     */
-    @Override
-    public void init() throws ServletException {
-        // TODO Auto-generated method stub
-        super.init();
-
-    }
-
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
@@ -59,8 +49,10 @@ public class HelloWorldServlet extends HttpServlet {
         out.write("<html>");
         out.write(String.format("<style type=\"text/css\">*{color:%s;}</style>", color));
         out.write("<h1>Hello the world!</h1>");
-        out.write("<em>2 lignes en JavaEE</em>");
-        out.write("<em>1 troisieme lignes en JavaEE</em>");
+        out.write("<em>2 lignes en JavaEE</em><br/>");
+        out.write("<em>1 troisieme lignes en JavaEE</em><br/>");
+        out.write("<a href=\"HelloWorldServlet?color=grey\">Same page in grey</a><br/>");
+        out.write("<a href=\"HelloWorldServlet?color=magenta\">Same page in magenta</a><br/>");
         out.write("</html>");
     }
 
@@ -70,5 +62,4 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
     }
-
 }
