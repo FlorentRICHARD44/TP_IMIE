7be2aef9f0635c0b892d0167d4dcfa8313ec5413
Florent RICHARD <florentrichard@hotmail.fr>
2015-07-14 10:54:30 +0200

    JPA: add task list (in root package-info)

--- TP_JavaEE/TPJavaEEIHM/src/fr/package-info.java
+++ TP_JavaEE/TPJavaEEIHM/src/fr/package-info.java
@@ -2,3 +2,7 @@
  * @author Florent RICHARD
  */
 package fr;
+// TODO HTML + CSS complet comme tp archi
+// TODO Ajout modif password
+// TODO Filtrer les usagers par site
+// TODO Lien + vers ajout de site en direct sur la fiche de usager (a coté de la combobox de site)
