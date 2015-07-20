9c2fc15b53dd7fe32c250da1932f0a71442614ec
Florent RICHARD <florentrichard@hotmail.fr>
2015-07-20 12:40:24 +0200

    JavaScript: utilisation directe dans la page HTML

--- TP_JavaScript/WebContent/exercice_01.js
+++ TP_JavaScript/WebContent/exercice_01.js
@@ -1,19 +1,21 @@
 /**
  * 
  */
-age = prompt("Donner l'âge de la personne");
-cat = ""
-if (age >= 6) {
-	if (age >= 6 & age <= 7) {
-		cat = "Poussin"
-	} else if (age >= 8 & age <= 9) {
-		cat = "Pupille"
-	} else if (age >= 10 & age <= 11) {
-		cat = "Minime"
+
+function calc_categorie(age, label) {
+	cat = ""
+	if (age >= 6) {
+		if (age >= 6 & age <= 7) {
+			cat = "Poussin"
+		} else if (age >= 8 & age <= 9) {
+			cat = "Pupille"
+		} else if (age >= 10 & age <= 11) {
+			cat = "Minime"
+		} else {
+			cat = "Cadet"
+		}
+		document.getElementById(label).innerHTML = "La catégorie est " + cat
 	} else {
-		cat = "Cadet"
+		document.getElementById(label).innerHTML = "La personne est trop jeune pour jouer"
 	}
-	alert("La catégorie est " + cat)
-} else {
-	alert("La personne est trop jeune pour jouer")
 }
--- TP_JavaScript/WebContent/exercice_02.js
+++ TP_JavaScript/WebContent/exercice_02.js
@@ -2,13 +2,14 @@
  * 
  */
 
-nbPhotocop = prompt("Entrer le nombre de photocopie")
-total = 0
-total += Math.min(10, nbPhotocop) * 0.10
-nbPhotocop -= Math.min(10, nbPhotocop)
-total += Math.min(20, nbPhotocop) * 0.08
-nbPhotocop -= Math.min(20, nbPhotocop)
-total += nbPhotocop * 0.05
-nbPhotocop -= nbPhotocop
-alert("Le prix total est de " + total)
+function calc_price_photocop(nbPhotocop, label) {
+	total = 0
+	total += Math.min(10, nbPhotocop) * 0.10
+	nbPhotocop -= Math.min(10, nbPhotocop)
+	total += Math.min(20, nbPhotocop) * 0.08
+	nbPhotocop -= Math.min(20, nbPhotocop)
+	total += nbPhotocop * 0.05
+	nbPhotocop -= nbPhotocop
+	document.getElementById(label).innerHTML = ''+ total
+}
 
